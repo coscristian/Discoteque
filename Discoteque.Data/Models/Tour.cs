7051265f4881d7050a33629c6b6070795a10172a
Cristian Quesada Cossio <cristian.quesada@utp.edu.co>
2023-09-15 00:39:27 -0500

    Adding non null values

--- Discoteque.Data/Models/Tour.cs
+++ Discoteque.Data/Models/Tour.cs
@@ -2,8 +2,8 @@ namespace Discoteque.Data.Models;
 
 public class Tour : BaseEntity<int>
 {
-    public string TourName {get; set;}
-    public string City {get; set;}
+    public string TourName {get; set;} = "";
+    public string City {get; set;} = "";
     public DateTime Date {get; set;}
     public bool IsSoldOut {get; set;}
     public int ArtistId {get; set;}
